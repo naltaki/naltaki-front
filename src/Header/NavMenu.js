7f242dcd8fc47363294073f2b2b57938efad4f25
Yacine Rezgui <rezgui.y@gmail.com>
2015-06-28 11:27:23 +0100

    Add NavMenu component

---
+++ src/Header/NavMenu.js
@@ -0,0 +1,23 @@
+import React from 'react/addons';
+
+export default class NavMenu extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderNavItem(item, index) {
+    if (item.link[0] === '/') {
+      return <a href={item.link} key={index}>{item.name}</a>
+    }
+    
+    return <a href={item.link} key={index} target="_blank">{item.name}</a>
+  }
+
+  render() {
+    return (
+      <nav className="masthead-nav">
+        {this.props.config.header_menu_links.map(this.renderNavItem)}
+      </nav>
+    );
+  }
+}
