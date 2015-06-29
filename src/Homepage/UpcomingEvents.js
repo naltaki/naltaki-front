56d6acd50ebb6939ba061b374bb9b21ce60c3356
Yacine Rezgui <rezgui.y@gmail.com>
2015-06-29 11:41:38 +0100

    Add UpcomingEvents component

---
+++ src/Homepage/UpcomingEvents.js
@@ -0,0 +1,37 @@
+import React from 'react/addons';
+import moment from 'moment';
+
+export default class UpcomingEvents extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderEventItem(item, index) {
+    return (
+      <li key={index}>
+        <span>{moment(item.attributes.startAt).format('MMMM, Do YYYY')} · </span>
+        <a href={item.attributes.venueLink} target="_blank">{item.attributes.venueName}</a>
+      </li>
+    );
+  }
+
+  render() {
+
+    if (!this.props.events || !this.props.events.length) {
+      return (
+        <div />
+      );
+    }
+
+    return (
+      <div>
+        <br />
+        <h2>Upcoming events</h2>
+        <ul>
+          {this.props.events.map(this.renderEventItem)}
+        </ul>
+        <br />
+      </div>
+    );
+  }
+}
