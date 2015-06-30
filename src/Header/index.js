7d4ad6b00bd443822b9d0503abdbf2632b434a4e
Yacine Rezgui <rezgui.y@gmail.com>
2015-06-30 13:47:55 +0100

    Use proper constant for logo link

--- src/Header/index.js
+++ src/Header/index.js
@@ -9,14 +9,14 @@ export default class Header extends React.Component {
   render() {
     let navMenu;
 
-    if (this.props.config.header_menu_links.length > 0) {
-      navMenu = <NavMenu config={this.props.config} />
+    if (this.props.config.headerMenuLinks.length > 0) {
+      navMenu = <NavMenu links={this.props.config.headerMenuLinks} />
     }
 
     return (
       <header className="masthead" style={{backgroundColor: '#4C5664'}}>
         <div className="container">
-          <a href="/" className="masthead-logo">{this.props.config.title}</a>
+          <a href={this.props.config.rootUrl} className="masthead-logo">{this.props.config.name}</a>
           {navMenu}
         </div>
       </header>
