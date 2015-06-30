0f203cdde5a960607be87de38252999ddf6794fe
Yacine Rezgui <rezgui.y@gmail.com>
2015-06-30 13:49:49 +0100

    Display Homepage component as main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react/addons';
 import Header from './Header';
 import TwitterSidebar from './widgets/TwitterSidebar';
+import Homepage from './Homepage';
 import CodeOfConduct from './CodeOfConduct';
+import Contact from './Contact';
+
+// <CodeOfConduct />
+// <Contact formLink={this.props.config.formContact} />
+// <Homepage description={this.props.config.description} />
 
 export default class App extends React.Component {
   constructor(props) {
@@ -15,7 +21,7 @@ export default class App extends React.Component {
         <div className="container">
           <div className="columns">
             <TwitterSidebar config={this.props.config} />
-            <CodeOfConduct />
+            <Homepage description={this.props.config.description} />
           </div>
         </div>
       </section>
