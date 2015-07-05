9720314a735fbccc96aae99037d65b3e485542d4
Yacine Rezgui <rezgui.y@gmail.com>
2015-07-05 14:45:03 +0100

    Simplifying config options

--- src/main.js
+++ src/main.js
@@ -8,16 +8,6 @@ Parse.initialize(window.PARSE_APPLICATION_ID, window.PARSE_JAVASCRIPT_KEY);
 
 var mountNode = document.getElementById('main');
 
-const config = {
-  name: window.GROUP_NAME,
-  description: window.GROUP_DESCRIPTION,
-  rootUrl: window.ROOT_URL,
-  formContact: window.FORM_CONTACT,
-  headerMenuLinks: window.HEADER_MENU_LINKS,
-  twitterUsername: window.TWITTER_USERNAME,
-  twitterWidgetId: window.TWITTER_WIDGET_ID
-};
-
 Router.run(routes, Router.HashLocation, (Root) => {
-  React.render(<Root config={config} />, mountNode);
+  React.render(<Root config={window.GROUP_CONFIG} />, mountNode);
 });
